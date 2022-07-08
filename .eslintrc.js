ac2d322b009b151ec9d21b8c782bb5ff246aeebe
b0jun <bjkim0228@naver.com>
2022-07-08 13:20:53 +0900

    chore: eslint prettier 초기 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    '@react-native-community',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   overrides: [
@@ -8,8 +14,38 @@ module.exports = {
       files: ['*.ts', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+        'no-console': 0,
+        'no-explicit-any': 0,
+        'import/extensions': 0,
+        'react/react-in-jsx-scope': 0,
+        'react/require-default-props': 0,
+        'react/function-component-definition': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        'import/no-unresolved': 'off',
+        'prettier/prettier': ['error', { endOfLine: 'auto', singleQuote: true, printWidth: 120, trailingComma: 'all' }],
+        'import/order': [
+          'error',
+          {
+            pathGroups: [
+              {
+                pattern: 'constants/**',
+                group: 'object',
+              },
+              {
+                pattern: 'assets/**',
+                group: 'object',
+              },
+            ],
+            groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+            'newlines-between': 'always',
+            warnOnUnassignedImports: true,
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+          },
+        ],
       },
     },
   ],
