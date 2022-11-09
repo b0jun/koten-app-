51cd638f6f452b510b2710b8bfc4aed9f0cfb62c
b0jun <bjkim0228@naver.com>
2022-11-09 20:57:30 +0900

    chore: filpper 설정

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -8,6 +8,15 @@ const rootReducer = {
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    if (__DEV__) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const reduxFlipper = require('redux-flipper').default;
+      return getDefaultMiddleware().concat(reduxFlipper());
+    } else {
+      return getDefaultMiddleware();
+    }
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
